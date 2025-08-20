eb782177ef11f7d2c0b90d27f6d398541ea2685e
dawar2151 <zouhirtaousy@gmail.com>
2025-08-20 19:12:42 +0200

    fix:  formatting sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,38 +1,38 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://ClaimYourSols.app'
-    const now = new Date().toISOString();
-    return [
-        {
-            url: `${baseUrl}/faq`,
-            lastModified: now,
-            changeFrequency: 'monthly',
-            priority: 0.6,
-        },
-        {
-            url: `${baseUrl}/support`,
-            lastModified: now,
-            changeFrequency: 'monthly',
-            priority: 0.5,
-        },
-        {
-            url: `${baseUrl}/terms`,
-            lastModified: now,
-            changeFrequency: 'monthly',
-            priority: 0.5,
-        },
-        {
-            url: `${baseUrl}/privacy`,
-            lastModified: now,
-            changeFrequency: 'monthly',
-            priority: 0.5,
-        },
-        {
-            url: `${baseUrl}/`,
-            lastModified: now,
-            changeFrequency: 'monthly',
-            priority: 0.5,
-        }
-    ]
-} 
+  const baseUrl = "https://ClaimYourSols.app";
+  const now = new Date().toISOString();
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/support`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/`,
+      lastModified: now,
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
